92a520e22bed3a95844a01da81e5c712a46b98cc
Dominik Frackowiak <86988772+DominikFrackowiak@users.noreply.github.com>
2024-11-16 21:30:58 +0100

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,18 @@
 import { defineConfig } from 'astro/config'
 import vercel from '@astrojs/vercel/serverless'
 import tailwind from '@astrojs/tailwind'
+import react from '@astrojs/react'
 
 export default defineConfig({
 	output: 'server',
-	integrations: [tailwind()],
+	integrations: [tailwind(), react()],
 	adapter: vercel(),
+	vite: {
+		// resolve: {
+		// 	alias: {
+		// 		react: 'react@16.13.0',
+		// 		'react-dom': 'react-dom@16.13.0',
+		// 	},
+		// },
+	},
 })
