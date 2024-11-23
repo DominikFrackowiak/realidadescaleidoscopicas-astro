2685242274195b452f83091be7d3ddd724254427
DominikFrackowiak <rorschach@op.pl>
2024-11-23 14:04:44 +0100

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
 
-import tailwind from '@astrojs/tailwind';
+import tailwind from '@astrojs/tailwind'
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'hybrid',
-  integrations: [tailwind()]
-});
+	server: { port: 3000 },
+	output: 'hybrid',
+	integrations: [tailwind()],
+})
