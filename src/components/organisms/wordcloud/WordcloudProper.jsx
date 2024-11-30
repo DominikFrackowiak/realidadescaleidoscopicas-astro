0b75f9c0657e5d5037842dff98101552086fbcfc
Dominik Frackowiak <86988772+DominikFrackowiak@users.noreply.github.com>
2024-11-30 22:05:12 +0100

    update

--- src/components/organisms/wordcloud/WordcloudProper.jsx
+++ src/components/organisms/wordcloud/WordcloudProper.jsx
@@ -1,34 +1,45 @@
-import React, { useEffect, useState } from 'react';
-import ReactWordcloud from 'react-wordcloud';
+'use client'
+
+import React, { useEffect, useState } from 'react'
+import ReactWordcloud from 'react-wordcloud'
 
 const words = [
-  { text: 'Astro', value: 100 },
-  { text: 'React', value: 80 },
-  { text: 'JavaScript', value: 70 },
-];
+	{ text: 'Astro', value: 100 },
+	{ text: 'React', value: 80 },
+	{ text: 'JavaScript', value: 70 },
+]
 
 const options = {
-  rotations: 2,
-  rotationAngles: [-90, 0],
-  fontSizes: [20, 60],
-};
-
+	colors: ['#fc3951', '#ed0033', '#364ec6', '#2e3192'],
+	enableTooltip: true,
+	deterministic: false,
+	fontFamily: 'Galano Grotesque Semibold',
+	fontSizes: [20, 70],
+	fontStyle: 'normal',
+	fontWeight: 'normal',
+	padding: 1,
+	rotations: 3,
+	rotationAngles: [0, 90],
+	scale: 'sqrt',
+	spiral: 'archimedean',
+	transitionDuration: 1000,
+}
 const WordCloudComponent = () => {
-  const [isClient, setIsClient] = useState(false);
+	const [isClient, setIsClient] = useState(false)
 
-  useEffect(() => {
-    setIsClient(true);
-  }, []);
+	useEffect(() => {
+		setIsClient(true)
+	}, [])
 
-  if (!isClient) {
-    return <div>Loading...</div>;
-  }
+	if (!isClient) {
+		return <div>Loading...</div>
+	}
 
-  return (
-    <div style={{ width: '100%', height: '400px' }}>
-      <ReactWordcloud words={words} options={options} />
-    </div>
-  );
-};
+	return (
+		<div style={{ width: '100%', height: '400px' }}>
+			<ReactWordcloud words={words} options={options} />
+		</div>
+	)
+}
 
-export default WordCloudComponent;
+export default WordCloudComponent
