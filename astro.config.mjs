2efa4370df562b0cfcaba7234589a6d92d4e5ed5
DominikFrackowiak <rorschach@op.pl>
2025-01-05 16:52:46 +0100

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,19 @@
-import { defineConfig } from 'astro/config'
-import vercel from '@astrojs/vercel/serverless'
-import tailwind from '@astrojs/tailwind'
-import react from '@astrojs/react'
+import { defineConfig } from 'astro/config';
+import vercel from '@astrojs/vercel';
+import tailwind from '@astrojs/tailwind';
+import react from '@astrojs/react';
 
 export default defineConfig({
-	output: 'server',
-	server: { port: 3000 },
-	integrations: [tailwind(), react()],
-	build: {
-		assets: 'assets',
-	},
-	adapter: vercel(),
-	vite: {
-		ssr: {
-			noExternal: ['react-wordcloud'],
-		},
-	},
-})
+  output: 'server',
+  server: { port: 3000 },
+  integrations: [tailwind(), react()],
+  build: {
+    assets: 'assets',
+  },
+  adapter: vercel(),
+  vite: {
+    ssr: {
+      noExternal: ['react-wordcloud'],
+    },
+  },
+});
