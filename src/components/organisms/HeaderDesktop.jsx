128ad091314ebfc48d62c7b68fb6d6a13abaa180
DominikFrackowiak <rorschach@op.pl>
2025-01-07 11:40:21 +0100

    update

--- src/components/organisms/HeaderDesktop.jsx
+++ src/components/organisms/HeaderDesktop.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 
 import { twMerge } from 'tailwind-merge';
 import { navLinks } from '../../../lib/constants/navigation';
